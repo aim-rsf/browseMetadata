a1dcde71a68d825bbbcca39238ccec585d4be03c
Rachael Stickland <rcstickland@gmail.com>
2023-09-19 14:49:01 +0100

    Better user interface

--- browseSAIL/R/domain_mapping.R
+++ browseSAIL/R/domain_mapping.R
@@ -1,40 +1,23 @@
 #' domain_mapping
 #'
-#' This function will read in the meta-data of a dataset (DataClass) from the SAIL databank, obtained from metadata catalogue (https://modelcatalogue.cs.ox.ac.uk/hdruk_live).
-#' It will loop through all the variable names, and ask you to categorise each variable into one of your chosen domains.
-#' Information about the Data Asset and Data Class will be displayed to the command window for reference.
-#' The domains will appear in the Plots tab, with the labels you should you for the categorisation.
-#' A log file will be saved with the categorisations you made.
-#' @param json_file The metadata file. This should be downloaded from the metadata catalogue as a json file.
-#' @param domain_file The file that lists the domains of interest to be used within the research study, provided as a csv with each domain on a separate line, within quotations.
-#' @param demo_mode Write TRUE to run the function in demo mode, using the demo data in the package, and leaving the other inputs blank. See example below.
-#' @return The function will return a log file with your chosen categorisations.
-#' @examples
-#' \strong{Run the code with the demo files}
-#'
-#' \emph{domain_mapping(,,TRUE)}
-#'
-#' It will first output a description of the data asset and data class that you have loaded in.
-#'
-#' It will ask you for a range of variables (data elements) to process from this data class.
-#' For this demo, write '1,10' so that the meta dataset file can be processed quickly.
-#' If you press enter, it will process all the variables in the dataset file (93).
-#'
-#' It will ask you for your initials.
-#' This is to save within the log file, to keep track of who made the categorizations.
-#'
-#' It will then loop through each variable you have requested.
-#' It will show you the name, description and data type for each variable.
-#' See the Plots tab for the Domain table.
-#' Respond to this prompt with a single number or multiple numbers separate by a column.
-#' The user has an option to write a note explaining their category choice.
-#'
-#' \strong{Run the code with your input files}
-#'
-#' \emph{domain_mapping(your-metadata.json,your-domain-list.csv)}
-#'
-#'
-#' @export
+#'This function will read in the meta-data of a Data Asset obtained from metadata catalogue: https://modelcatalogue.cs.ox.ac.uk/hdruk_live.
+#'It will loop through all the Data Elements (variable names) in each Data Class (table), and ask you to categorise each variable into one of your chosen domains.
+#'Information about the Data Asset and Data Class can be displayed to the command window for reference.
+#'The domains will appear in the Plots tab, with the labels you should use for the categorisation.
+#'@param json_file The metadata file. This should be downloaded from the metadata catalogue as a json file.
+#'@param domain_file The file that lists the domains of interest to be used within the research study, provided as a csv with each domain on a separate line, within quotations.
+#'@param demo_mode Write TRUE to run the function in demo mode and leave the other inputs blank. This uses the demo data in the package - see example below.
+#'@return The function will return a log file with your mapping between variables and domains, alongside details about the Data Asset.
+#'@examples
+#'# Run the code with the demo files
+#'\emph{domain_mapping(,,TRUE)}
+#'# Respond with your initials when prompted.
+#'# Respond 'Demo List.' for the description of demo list.
+#'# Respond 'Y' if you want to see the descriptions printed out.
+#'# Respond '1,10' to the RANGE OF VARIABLES prompt (or process the full 93 variables if you like!)
+#'# Reference the plot tab and categorise each variable into a single ('1') or multiple  ('1,2') domain.
+#'# Write a note explaining your category choice (optional).
+#'@export
 domain_mapping <- function(json_file,domain_file,demo_mode = FALSE) {
 
   library(rjson)
@@ -62,58 +45,7 @@ domain_mapping <- function(json_file,domain_file,demo_mode = FALSE) {
   domains_extend <- rbind(c('*NO MATCH / UNSURE*'),c('*METADATA*'), c('*ALF ID*'),c('*OTHER ID*'),c('*DEMOGRAPHICS*'),domains)
   grid.table(domains_extend[1],cols='Domain',rows=0:(nrow(domains_extend)-1))
 
-  # Print information about Data Asset and Class ----
-  print_colour("\nData Asset Name --- Last Updated \n",'br_violet')
-  cat(meta_json$dataModel$label,"---",meta_json$dataModel$lastUpdated,fill=TRUE)
-  print_colour("Data Asset Description \n",'br_violet')
-  cat(meta_json$dataModel$description,fill=TRUE)
-  print_colour("Data Asset Exported \n",'br_violet')
-  cat("By", meta_json$exportMetadata$exportedBy, "at", meta_json$exportMetadata$exportedOn,"\n",fill=TRUE)
-  print_colour("\nData Class Name --- Last Updated \n",'br_violet')
-  cat(meta_json$dataModel$childDataClasses[[1]]$label,'---',meta_json$dataModel$childDataClasses[[1]]$lastUpdated,fill=TRUE)
-  print_colour("Data Class Description \n",'br_violet')
-  cat(meta_json$dataModel$childDataClasses[[1]]$description,fill=TRUE)
-
-  # Extract DataClass (probably a better way of dealing with jsons in R ...)
-  thisDataClass <- meta_json$dataModel$childDataClasses[[1]]$childDataElements
-  thisDataClass_df <- data.frame(do.call(rbind,thisDataClass)) # nested list to dataframe
-  dataType_df <- data.frame(do.call(rbind,thisDataClass_df$dataType)) # nested list to dataframe
-
-  selectDataClass_df <- data.frame (Label  = unlist(thisDataClass_df$label),
-                                    Description = unlist(thisDataClass_df$description),
-                                    Type = unlist(dataType_df$label)
-                                    )
-
-  selectDataClass_df <- selectDataClass_df[order(selectDataClass_df$Label),]
-
-  # Create unique output csv to log the results ----
-  timestamp_now <- gsub(" ", "_",Sys.time())
-  timestamp_now <- gsub(":", "-",timestamp_now)
-  output_fname <- paste0("LOG-FILE_",timestamp_now,".csv")
-
-  Output <- data.frame(Initials = c(""),
-                       MetaDataVersion = c(""),
-                       MetaDataLastUpdated = c(""),
-                       DomainListDesc = c(""),
-                       DataAsset = c(""),
-                       DataClass = c(""),
-                       DataElement = c(""),
-                       Domain_code = c(""),
-                       Note = c("")
-    )
-  # User inputs ----
-
-  cat("\n \n")
-  select_vars_n <- readline(prompt="RANGE OF VARIABLES (DATA ELEMENTS) TO PROCESS (write as 'start_var,end_var' or press Enter to process all): ")
-  if (select_vars_n == "") {
-    start_var <- 1
-    end_var <- length(thisDataClass)
-  } else {
-    seperate_vars <- unlist(strsplit(select_vars_n,","))
-    start_var <- as.numeric(seperate_vars[1])
-    end_var <- as.numeric(seperate_vars[2])
-  }
-
+  # Get user and demo list info for log file ----
   User_Initials <- ""
   while (User_Initials == "") {
     cat("\n \n")
@@ -126,84 +58,165 @@ domain_mapping <- function(json_file,domain_file,demo_mode = FALSE) {
     DomainListDesc <- readline(prompt="PROVIDE SOME DESCRIPTION OF DOMAIN LIST USED (version number, created by): ")
   }
 
-  # Loop through each variable, request response from the user to match to a domain ----
-  for  (datavar in start_var:end_var ) {
+  # Print information about Data Asset ----
+  print_colour("\nData Asset Name \n",'br_violet')
+  cat(meta_json$dataModel$label,fill=TRUE)
+  print_colour("Data Asset Last Updated \n",'br_violet')
+  cat(meta_json$dataModel$lastUpdated,fill=TRUE)
+  print_colour("Data Asset Exported \n",'br_violet')
+  cat("By", meta_json$exportMetadata$exportedBy, "at", meta_json$exportMetadata$exportedOn,fill=TRUE)
+  nDataClasses <- length(meta_json$dataModel$childDataClasses)
+  print_colour(sprintf("There are %s Data Classes (tables) in this Data Asset",nDataClasses),'br_violet')
 
-    # auto categorise
-    if (grepl("AVAIL_FROM_DT", selectDataClass_df$Label[datavar])) {
+  dataasset_desc <- readline(prompt="Would you like to read a description of the Data Asset? (Y/N) ")
+  if (dataasset_desc == "Y") {
+    print_colour("Data Asset Description \n",'br_violet')
+    cat(meta_json$dataModel$description,fill=TRUE)
+    readline(prompt="Press [enter] to proceed")
+  }
 
-      Output [ nrow(Output) + 1 , ] <- NA
-      Output$DataElement[datavar]
-      Output$DataElement[datavar] <- selectDataClass_df$Label[datavar]
-      Output$Domain_code[datavar] <- '1'
-      Output$Note[datavar] <- 'AUTO CATEGORISED'
+  # Extract each DataClass (Table)
+  for (dc in 1:nDataClasses) {
+    print_colour(sprintf("\n\nProcessing Data Class (Table) %s of %s \n",dc,nDataClasses),'br_violet')
+    print_colour("\nData Class Name \n",'br_violet')
+    cat(meta_json$dataModel$childDataClasses[[dc]]$label,fill=TRUE)
+    print_colour("Data Class Last Updated \n",'br_violet')
+    cat(meta_json$dataModel$childDataClasses[[dc]]$lastUpdated,fill=TRUE)
 
-    } else if (grepl("ALF", selectDataClass_df$Label[datavar])) {
+    dataclass_desc <- readline(prompt="Would you like to read a description of the Data Class (Table)? (Y/N) ")
+    if (dataclass_desc == "Y") {
+      print_colour("Data Class Description \n",'br_violet')
+      cat(meta_json$dataModel$childDataClasses[[dc]]$description,fill=TRUE)
+      readline(prompt="Press [enter] to proceed")
+    }
 
-      Output [ nrow(Output) + 1 , ] <- NA
-      Output$DataElement[datavar] <- selectDataClass_df$Label[datavar]
-      Output$Domain_code[datavar] <- '2'
-      Output$Note[datavar] <- 'AUTO CATEGORISED'
+    thisDataClass <- meta_json$dataModel$childDataClasses[[dc]]$childDataElements #  probably a better way of dealing with complex json files in R ...
+    thisDataClass_df <- data.frame(do.call(rbind,thisDataClass)) # nested list to dataframe
+    dataType_df <- data.frame(do.call(rbind,thisDataClass_df$dataType)) # nested list to dataframe
 
-    } else if (grepl("_ID_", selectDataClass_df$Label[datavar])) {
+    selectDataClass_df <- data.frame (Label  = unlist(thisDataClass_df$label),
+                                      Description = unlist(thisDataClass_df$description),
+                                      Type = unlist(dataType_df$label)
+    )
 
-      Output [ nrow(Output) + 1 , ] <- NA
-      Output$DataElement[datavar] <- selectDataClass_df$Label[datavar]
-      Output$Domain_code[datavar] <- '3'
-      Output$Note[datavar] <- 'AUTO CATEGORISED'
+    selectDataClass_df <- selectDataClass_df[order(selectDataClass_df$Label),]
 
-    } else if (grepl("AGE", selectDataClass_df$Label[datavar])
-               || grepl("DOB", selectDataClass_df$Label[datavar])
-               || grepl("WOB", selectDataClass_df$Label[datavar])
-               || grepl("ETHNIC", selectDataClass_df$Label[datavar])
-               || grepl("SEX", selectDataClass_df$Label[datavar])
-               || grepl("GENDER", selectDataClass_df$Label[datavar])) {
+    # Create unique output csv to log the results ----
+    timestamp_now <- gsub(" ", "_",Sys.time())
+    timestamp_now <- gsub(":", "-",timestamp_now)
 
-      Output [ nrow(Output) + 1 , ] <- NA
-      Output$DataElement[datavar] <- selectDataClass_df$Label[datavar]
-      Output$Domain_code[datavar] <- '4'
-      Output$Note[datavar] <- 'AUTO CATEGORISED'
+    output_fname <- paste0("LOG_",gsub(" ", "",meta_json$dataModel$label),'_',gsub(" ", "",meta_json$dataModel$childDataClasses[[dc]]$label),'_',timestamp_now,".csv")
 
+    Output <- data.frame(Initials = c(""),
+                         MetaDataVersion = c(""),
+                         MetaDataLastUpdated = c(""),
+                         DomainListDesc = c(""),
+                         DataAsset = c(""),
+                         DataClass = c(""),
+                         DataElement = c(""),
+                         Domain_code = c(""),
+                         Note = c("")
+    )
+
+    # User inputs ----
+
+    cat("\n \n")
+    select_vars_n <- readline(prompt="RANGE OF VARIABLES (DATA ELEMENTS) TO PROCESS (write as 'start_var,end_var' or press Enter to process all): ")
+    if (select_vars_n == "") {
+      start_var <- 1
+      end_var <- length(thisDataClass)
     } else {
+      seperate_vars <- unlist(strsplit(select_vars_n,","))
+      start_var <- as.numeric(seperate_vars[1])
+      end_var <- as.numeric(seperate_vars[2])
+    }
 
-      # user response
-      cat(paste("\nDATA ELEMENT -----> ",selectDataClass_df$Label[datavar],"\n\nDESCRIPTION -----> ",selectDataClass_df$Description[datavar],"\n\nDATA TYPE -----> ",selectDataClass_df$Type[datavar],"\n"))
+    # Loop through each variable, request response from the user to match to a domain ----
+    for  (datavar in start_var:end_var ) {
 
-      decision <- ""
-      while (decision == "") {
-        cat("\n \n")
-        decision <- readline(prompt="CATEGORISE THIS VARIABLE (input a comma seperated list of domain numbers): ")
+      # auto categorise
+      if (grepl("NA", selectDataClass_df$Label[datavar])) {
+
+        Output [ nrow(Output) + 1 , ] <- NA
+        Output$DataElement[datavar]
+        Output$DataElement[datavar] <- selectDataClass_df$Label[datavar]
+        Output$Domain_code[datavar] <- '0'
+        Output$Note[datavar] <- 'AUTO CATEGORISED'
+
+      } else if (grepl("AVAIL_FROM_DT", selectDataClass_df$Label[datavar])) {
+
+        Output [ nrow(Output) + 1 , ] <- NA
+        Output$DataElement[datavar]
+        Output$DataElement[datavar] <- selectDataClass_df$Label[datavar]
+        Output$Domain_code[datavar] <- '1'
+        Output$Note[datavar] <- 'AUTO CATEGORISED'
+
+      } else if (grepl("ALF", selectDataClass_df$Label[datavar])) {
+
+        Output [ nrow(Output) + 1 , ] <- NA
+        Output$DataElement[datavar] <- selectDataClass_df$Label[datavar]
+        Output$Domain_code[datavar] <- '2'
+        Output$Note[datavar] <- 'AUTO CATEGORISED'
+
+      } else if (grepl("_ID_", selectDataClass_df$Label[datavar])) {
+
+        Output [ nrow(Output) + 1 , ] <- NA
+        Output$DataElement[datavar] <- selectDataClass_df$Label[datavar]
+        Output$Domain_code[datavar] <- '3'
+        Output$Note[datavar] <- 'AUTO CATEGORISED'
+
+      } else if (grepl("AGE", selectDataClass_df$Label[datavar])
+                 || grepl("DOB", selectDataClass_df$Label[datavar])
+                 || grepl("WOB", selectDataClass_df$Label[datavar])
+                 || grepl("ETHNIC", selectDataClass_df$Label[datavar])
+                 || grepl("SEX", selectDataClass_df$Label[datavar])
+                 || grepl("GENDER", selectDataClass_df$Label[datavar])) {
+
+        Output [ nrow(Output) + 1 , ] <- NA
+        Output$DataElement[datavar] <- selectDataClass_df$Label[datavar]
+        Output$Domain_code[datavar] <- '4'
+        Output$Note[datavar] <- 'AUTO CATEGORISED'
+
+      } else {
+
+        # user response
+        cat(paste("\nDATA ELEMENT -----> ",selectDataClass_df$Label[datavar],"\n\nDESCRIPTION -----> ",selectDataClass_df$Description[datavar],"\n\nDATA TYPE -----> ",selectDataClass_df$Type[datavar],"\n"))
+
+        decision <- ""
+        while (decision == "") {
+          cat("\n \n")
+          decision <- readline(prompt="CATEGORISE THIS VARIABLE (input a comma seperated list of domain numbers): ")
+        }
+
+        decision_note <- ""
+        while (decision_note == "") {
+          cat("\n \n")
+          decision_note <- readline(prompt="NOTES (write 'No' if no notes): ")
+        }
+
+        Output [ nrow(Output) + 1 , ] <- NA
+        Output$DataElement[datavar] <- selectDataClass_df$Label[datavar]
+        Output$Domain_code[datavar] <- decision
+        Output$Note[datavar] <- decision_note
+
       }
-
-      decision_note <- ""
-      while (decision_note == "") {
-        cat("\n \n")
-        decision_note <- readline(prompt="NOTES (write 'No' if no notes): ")
-      }
-
-      Output [ nrow(Output) + 1 , ] <- NA
-      Output$DataElement[datavar] <- selectDataClass_df$Label[datavar]
-      Output$Domain_code[datavar] <- decision
-      Output$Note[datavar] <- decision_note
 
     }
 
+    # Fill in columns that have all rows identical
+    Output$Initials = User_Initials
+    Output$MetaDataVersion = meta_json$dataModel$documentationVersion
+    Output$MetaDataLastUpdated = meta_json$dataModel$lastUpdated
+    Output$DomainListDesc = DomainListDesc
+    Output$DataAsset = meta_json$dataModel$label
+    Output$DataClass = meta_json$dataModel$childDataClasses[[dc]]$label
+
+    # Save file & print the responses to be saved
+    Output[Output == ''] <- NA
+    write.csv(Output, output_fname, row.names=FALSE)  #save as we go in case session terminates prematurely
+    print_colour(paste("\n \n The below responses will be saved to", output_fname,"\n \n"),'blue')
+    print(Output[,c("DataClass","DataElement","Domain_code","Note")])
   }
-
-  # Fill in columns that have all rows identical
-  Output$Initials = User_Initials
-  Output$MetaDataVersion = meta_json$dataModel$documentationVersion
-  Output$MetaDataLastUpdated = meta_json$dataModel$lastUpdated
-  Output$DomainListDesc = DomainListDesc
-  Output$DataAsset = meta_json$dataModel$label
-  Output$DataClass = meta_json$dataModel$childDataClasses[[1]]$label
-
-  # Save file & print the responses to be saved
-   Output[Output == ''] <- NA
-   write.csv(Output, output_fname, row.names=FALSE)  #save as we go in case session terminates prematurely
-   print_colour(paste("\n \n The below responses will be saved to", output_fname,"\n \n"),'blue')
-   print(Output)
-
 }
 
 
