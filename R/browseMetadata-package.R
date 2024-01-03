dae316985aca257cd22ced3408e4235faca91ada
BatoolMM <batool@liverpool.ac.uk>
2024-01-03 14:56:30 +0300

    remove ggplot2 not needed

--- R/browseMetadata-package.R
+++ R/browseMetadata-package.R
@@ -2,7 +2,7 @@
 #'
 #' Maps domains to varaibles within a dataset such as SAIL databank.
 #'
-#' @import ggplot2 devtools grid gridExtra insight rjson
+#' @import devtools grid gridExtra insight rjson
 #' @importFrom graphics plot.new
 #' @importFrom utils read.csv write.csv
 #' @keywords internal
