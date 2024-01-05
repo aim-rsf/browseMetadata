6f8b683592aef16030c0acaf85662609a74f2c62
Rachael Stickland <50215726+RayStick@users.noreply.github.com>
2024-01-05 14:11:15 +0000

    add json docs

---
+++ R/data-domain_list.R
@@ -0,0 +1,16 @@
+#' List of domains
+#'
+#' A simplified list of domains, to demo the function domain_mapping.R \cr \cr
+#' This data was created with these two steps:
+#' \enumerate{
+#'  \item \code{domain_list <- read.csv('browseMetadata/data-raw/domain_list_demo.csv')}
+#'  \item \code{usethis::use_data(domain_list)}
+#' }
+#' @docType data
+#
+#' @usage data(domain_list)
+#'
+#' @format A data frame with 5 rows and 1 column
+#' 
+#' @source The csv was manually created
+"domain_list"
---
+++ R/data-json_metadata.R
@@ -0,0 +1,20 @@
+#' Json metadata file
+#'
+#' Example metadata for a health dataset, to demo the function domain_mapping.R \cr \cr
+#' This data was created with these five steps:
+#' \enumerate{
+#'   \item Go to https://modelcatalogue.cs.ox.ac.uk/hdruk_live/#/catalogue/dataModel/17e86f3f-ec29-4c8e-9efc-8793a74b107d 
+#'   \item Download json metadata file by selecting the 'Export as JSON' option on the download button
+#'   \item \code{install.packages("rjson")}
+#'   \item \code{json_metadata <- rjson::fromJSON(file = '/browseMetadata/data-raw/maternity_indicators_dataset_(mids)_20240105T132210.json')}
+#'   \item \code{usethis::use_data(json_metadata)}
+#' }
+#'
+#' @docType data
+#
+#' @usage data(json_metadata)
+#'
+#' @format Nested lists
+#'
+#' @source https://modelcatalogue.cs.ox.ac.uk/hdruk_live/#/catalogue/dataModel/17e86f3f-ec29-4c8e-9efc-8793a74b107d
+"json_metadata"
