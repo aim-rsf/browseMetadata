65cb36b2c723e8bdee10534e8fcba448b0f93b2a
Rachael Stickland <50215726+RayStick@users.noreply.github.com>
2024-07-08 14:59:41 +0100

    supress regular warnings

--- R/user_categorisation.R
+++ R/user_categorisation.R
@@ -38,8 +38,8 @@ user_categorisation <- function(data_element,data_desc,data_type,domain_code_max
       # validate input given by user
       decision_int <- as.integer(unlist(strsplit(decision,",")))
       decision_int_NA <- any(is.na((decision_int)))
-      decision_int_max <- max(decision_int,na.rm=TRUE)
-      decision_int_min <- min(decision_int,na.rm=TRUE)
+      suppressWarnings(decision_int_max <- max(decision_int,na.rm=TRUE))
+      suppressWarnings(decision_int_min <- min(decision_int,na.rm=TRUE))
       if (decision_int_NA == TRUE | decision_int_max > domain_code_max | decision_int_min < 0){
         cli_alert_warning("Formatting is invalid or integer out of range. Provide one integer or a comma seperated list of integers.")
         validated = FALSE}
