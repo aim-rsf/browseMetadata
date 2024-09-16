6e8cdc61c49fa9a699029d62935b0562a12fd4b1
Rachael Stickland <50215726+RayStick@users.noreply.github.com>
2024-09-16 12:32:21 +0100

    check imports

--- R/user_prompt_list.R
+++ R/user_prompt_list.R
@@ -8,7 +8,7 @@
 #' @param list_allowed A list of allowable integer responses.
 #' @param empty_allowed A boolean specifying if no response is allowed.
 #' @return It returns a list of integers to process, that match the prompt options.
-#' @importFrom CHECK LATER
+#'
 
 user_prompt_list <- function(prompt_text,list_allowed,empty_allowed) {
 
@@ -17,7 +17,7 @@ user_prompt_list <- function(prompt_text,list_allowed,empty_allowed) {
   while (list_to_process_Error==TRUE | list_to_process_InRange==FALSE) {
     tryCatch({
       cat("\n \n");
-      cli_alert_info(prompt_text);
+      cli::cli_alert_info(prompt_text);
       cat("\n");
       list_to_process <- scan(file="",what=0);
       list_to_process_InRange_1 = (all(list_to_process %in% list_allowed))
@@ -25,9 +25,9 @@ user_prompt_list <- function(prompt_text,list_allowed,empty_allowed) {
         list_to_process_InRange_2 = (all(length(list_to_process) != 0))
       } else {list_to_process_InRange_2 = TRUE}
       list_to_process_InRange = all(list_to_process_InRange_1,list_to_process_InRange_2)
-      if (list_to_process_InRange == FALSE){cli_alert_danger('One of your inputs is out of range! Reference the allowable list of integers and try again.')};
+      if (list_to_process_InRange == FALSE){cli::cli_alert_danger('One of your inputs is out of range! Reference the allowable list of integers and try again.')};
       list_to_process_Error <- FALSE},
-      error=function(e) {list_to_process_Error <- TRUE; print(e); cat("\n"); cli_alert_danger('Your input is in the wrong format. Reference the allowable list of integers and try again.')})
+      error=function(e) {list_to_process_Error <- TRUE; print(e); cat("\n"); cli::cli_alert_danger('Your input is in the wrong format. Reference the allowable list of integers and try again.')})
     }
   list_to_process
 }
