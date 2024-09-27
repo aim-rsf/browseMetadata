8df5f57e089d481263fc7d84add177a99fe0ee5b
Rachael Stickland <50215726+RayStick@users.noreply.github.com>
2024-09-27 15:44:32 +0100

    arg not needed

--- R/ref_plot.R
+++ R/ref_plot.R
@@ -14,7 +14,7 @@ ref_plot <- function(domains){
   plot.new()
   domains_extend <- rbind(c("*NO MATCH / UNSURE*"), c("*METADATA*"), c("*ID*"), c("*DEMOGRAPHICS*"), domains)
   Code <- data.frame(Code = 0:(nrow(domains_extend) - 1))
-  Domain_table <- tableGrob(cbind(Code,domains_extend),rows = NULL,theme = ttheme_default())
+  Domain_table <- tableGrob(cbind(Code,domains_extend),rows = NULL)
   grid.arrange(Domain_table,nrow=1,ncol=1)
 
   return(list(Code = Code, Domain_table = Domain_table))
